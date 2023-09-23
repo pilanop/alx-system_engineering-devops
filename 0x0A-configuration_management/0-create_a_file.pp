2ab413a9dbe2701573a0e0e1bf254279f06735bb
Khalid Mohammed <khalarasta104@gmail.com>
2023-09-24 02:38:32 +0300

    0. Create a file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# creates a file called 'school'
+file { 'school':
+  content => 'I love Puppet',
+  group   => 'www-data',
+  mode    => '0744',
+  owner   => 'www-data',
+  path    => '/tmp/school',
+}
